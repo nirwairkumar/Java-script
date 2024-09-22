4b595894a7c691e86e20f1222721524792daefa7
Nirwair Kumar <119248367+nirwairkumar@users.noreply.github.com>
2024-09-23 03:01:34 +0530

    Add files via upload

---
+++ Part-B/appB.js
@@ -0,0 +1,29 @@
+for (i=1; i<=5; i++){
+    console.log(i);
+}
+
+// infinite loop
+// for (i=1;i>=0;i=i+2){
+//     console.log(i);
+// }
+
+// for (i=5 ;i<=50;i=i+5){
+//         console.log(i);
+// }
+
+
+let j= 1;
+while (j <=5){
+    console.log(j);
+    j++;
+}
+
+
+let fruits =["mango", "apple", "banana", "litchi", "orange"];
+for(fruit of fruits){
+    console.log(fruit);
+}
+
+for (char of "Nirwair"){
+    console.log(char);
+}
