8c06b781a9ecd45429e9f1a6ffe883f22b82192b
Nirwair Kumar <119248367+nirwairkumar@users.noreply.github.com>
2024-09-28 16:33:14 +0530

    Add files via upload

---
+++ Part-D/arrowFunction.js
@@ -0,0 +1,12 @@
+const sum = (a, b) => {
+    console.log(a+b);
+}
+sum(4,5);  //calling function
+
+const cube = (a) => {
+    console.log(a*a*a);
+}
+
+
+// implicit return
+const mul = (a,b) => a*b;
